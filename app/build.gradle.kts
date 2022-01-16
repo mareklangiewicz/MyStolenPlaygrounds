9d5e7053a8016ce0ded150ffd3569f25e8b6c647
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-01-16 18:08:19 +0100

    fun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,18 @@ plugins {
     kotlin("android") version Vers.kotlin
 }
 
-buildscript {
-    defaultAndroBuildScript()
-}
+buildscript { defaultAndroBuildScript() }
 
-repositories {
-    defaultRepositories()
+repositories { defaultRepositories() }
+
+android { defaultAndroid("pl.mareklangiewicz.playgrounds", withCompose = true) }
+
+dependencies {
+    implementation(project(":lib1"))
+    implementation(project(":lib-ui-samples"))
+    defaultAndroidDeps(withCompose = true)
+    defaultAndroidTestDeps(configuration = "implementation", withCompose = true)
+    // I use test stuff in main sources so I can add some tests sources to playgrounds app
 }
 
 group = "pl.mareklangiewicz.playgrounds"
@@ -21,37 +27,3 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
     }
 }
 
-android {
-    defaultAndroid("pl.mareklangiewicz.playgrounds", withCompose = true)
-}
-
-
-dependencies {
-    implementation(project(":lib1"))
-    implementation(project(":lib-ui-samples"))
-    implementation (Deps.androidxCoreKtx)
-    implementation (Deps.androidxAppcompat)
-    implementation (Deps.androidMaterial)
-    implementation (Deps.composeAndroidUi)
-    implementation (Deps.composeAndroidUiTooling)
-    implementation (Deps.composeAndroidMaterial3)
-    implementation (Deps.composeAndroidMaterial)
-    implementation (Deps.androidxLifecycleCompiler)
-    implementation (Deps.androidxLifecycleRuntimeKtx)
-    implementation (Deps.androidxActivityCompose)
-    // I use test stuff in main sources so I can add some tests sources to playgrounds app
-//    implementation (Deps.uspekx)
-    implementation (Deps.junit4)
-    implementation (Deps.androidxEspressoCore)
-    implementation (Deps.googleTruth)
-    implementation (Deps.androidxTestRules)
-    implementation (Deps.androidxTestRunner)
-    implementation (Deps.androidxTestExtTruth)
-    implementation (Deps.androidxTestExtJUnit)
-    implementation (Deps.composeAndroidUiTest)
-    implementation (Deps.composeAndroidUiTestJUnit4)
-    implementation (Deps.composeAndroidUiTestManifest)
-    implementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-//    implementation(Deps.mockitoKotlin2)
-    implementation(Deps.mockitoAndroid)
-}
--- lib-ui-samples/build.gradle.kts
+++ lib-ui-samples/build.gradle.kts
@@ -3,12 +3,15 @@ plugins {
     kotlin("android") version Vers.kotlin
 }
 
-buildscript {
-    defaultAndroBuildScript()
-}
+buildscript { defaultAndroBuildScript() }
 
-repositories {
-    defaultRepositories()
+repositories { defaultRepositories() }
+
+android { defaultAndroid(withCompose = true) }
+
+dependencies {
+    defaultAndroidDeps(withCompose = true)
+    defaultAndroidTestDeps(configuration = "implementation", withCompose = true)
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
@@ -16,36 +19,4 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
         jvmTarget = defaultJvmVersion
         freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
     }
-}
-
-android {
-    defaultAndroid(withCompose = true)
-}
-
-dependencies {
-    implementation (Deps.androidxCoreKtx)
-    implementation (Deps.androidxAppcompat)
-    implementation (Deps.androidMaterial)
-    implementation (Deps.composeAndroidUi)
-    implementation (Deps.composeAndroidUiTooling)
-    implementation (Deps.composeAndroidMaterial3)
-    implementation (Deps.composeAndroidMaterial)
-    implementation (Deps.androidxLifecycleCompiler)
-    implementation (Deps.androidxLifecycleRuntimeKtx)
-    implementation (Deps.androidxActivityCompose)
-    // I use test stuff in main sources so I can add some tests sources to playgrounds app
-//    implementation (Deps.uspekx)
-    implementation (Deps.junit4)
-    implementation (Deps.androidxEspressoCore)
-    implementation (Deps.googleTruth)
-    implementation (Deps.androidxTestRules)
-    implementation (Deps.androidxTestRunner)
-    implementation (Deps.androidxTestExtTruth)
-    implementation (Deps.androidxTestExtJUnit)
-    implementation (Deps.composeAndroidUiTest)
-    implementation (Deps.composeAndroidUiTestJUnit4)
-    implementation (Deps.composeAndroidUiTestManifest)
-    implementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-//    implementation(Deps.mockitoKotlin2)
-    implementation(Deps.mockitoAndroid)
 }
