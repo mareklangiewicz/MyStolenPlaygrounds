f0bae98aa96c29cfb71eaa8a55bcd6538a1ed1d5
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-04-13 13:02:21 +0200

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -303,7 +303,7 @@ val ureContentWithTemplate = ure {
  * * MYKOTLIBS_ossrhPassword
  * * MYKOTLIBS_sonatypeStagingProfileId
  * * First three of these used in fun pl.mareklangiewicz.defaults.defaultSigning
- * * See template-mpp/lib/build.gradle.kts
+ * * See deps.kt/template-mpp/template-mpp-lib/build.gradle.kts
  */
 fun Project.defaultSonatypeOssStuffFromSystemEnvs(envKeyMatchPrefix: String = "MYKOTLIBS_") {
     ext.addAllFromSystemEnvs(envKeyMatchPrefix)
