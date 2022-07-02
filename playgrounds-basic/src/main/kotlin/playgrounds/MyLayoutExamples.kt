d572092cf09296623cfb691000f8fceac0a0fa0c
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-07-02 14:14:43 +0200

    fun

--- playgrounds-basic/src/main/kotlin/playgrounds/MyLayoutExamples.kt
+++ playgrounds-basic/src/main/kotlin/playgrounds/MyLayoutExamples.kt
@@ -14,11 +14,17 @@ import androidx.compose.ui.unit.*
 import pl.mareklangiewicz.utheme.*
 import pl.mareklangiewicz.uwidgets.*
 import pl.mareklangiewicz.uwidgets.UAlignmentType.*
+import pl.mareklangiewicz.uwidgets.UContainerType.*
 import kotlin.Pair
 
 
+
+@Preview @Composable fun MyUBoxPreview() = MyLayoutExample1(UBOX)
+@Preview @Composable fun MyURowPreview() = MyLayoutExample1(UROW)
+@Preview @Composable fun MyUColumnPreview() = MyLayoutExample1(UCOLUMN)
+
 @Composable
-fun MyLayoutExample1() {
+fun MyLayoutExample1(type: UContainerType = UBOX) {
 
     val reports = remember { mutableStateListOf<Pair<String, Any>>() }
     fun report(v: Pair<String, Any>) {
@@ -26,15 +32,57 @@ fun MyLayoutExample1() {
         reports.add(v)
     }
 
-    Column(Modifier.fillMaxSize().background(Color.LightGray)) {
-        Box(Modifier.requiredSize(400.dp)) { MyExaminedLayout(::report) }
-        ReportsUi(reports)
+    Column(Modifier.fillMaxWidth().background(Color.LightGray)) {
+        Box(Modifier.border(4.dp, Color.Blue).padding(4.dp).requiredSize(360.dp)) {
+            MyExaminedLayout1(type, report = ::report)
+        }
+        ReportsUi(Modifier.height(400.dp), reports)
     }
 }
 
-@Composable fun ReportsUi(reports: List<Pair<String, Any>>) {
+@Composable
+fun MyExaminedLayout1(type: UContainerType = UBOX, addStretchedBlueBox: Boolean = false, report: (Pair<String, Any>) -> Unit) {
+    UAlign(USTART, USTART) {
+        when (type) {
+            UBOX -> UBasicBox { MyExaminedLayout1Content(addStretchedBlueBox, report) }
+            UROW -> UBasicRow { MyExaminedLayout1Content(addStretchedBlueBox, report) }
+            UCOLUMN -> UBasicColumn { MyExaminedLayout1Content(addStretchedBlueBox, report) }
+        }
+    }
+}
+
+@Composable
+private fun MyExaminedLayout1Content(addStretchedBlueBox: Boolean = false, report: (Pair<String, Any>) -> Unit) {
+    UAlign(USTART, UEND) { ExamBasicBox("magenta 160dp box", Color.Magenta, 160.dp.square, report = report) }
+    UAlign(UCENTER, UCENTER) { ExamBasicBox("red 80dp box", Color.Red, 80.dp.square, sizeRequired = true, report = report) }
+    UAlign(UEND, UEND) { ExamBasicBox("green 60dp box", Color.Green, 60.dp.square, report = report) }
+    if (addStretchedBlueBox) UAlign(USTRETCH, USTRETCH) { ExamBasicBox("blue 30dp stretched box", Color.Blue, 30.dp.square, report = report) }
+}
+
+val Dp.square get() = DpSize(this, this)
+
+@Composable
+fun ExamBasicBox(
+    tag: String,
+    color: Color = Color.Gray,
+    size: DpSize = 100.dp.square,
+    sizeRequired: Boolean = false,
+    report: (Pair<String, Any>) -> Unit = { println("${it.first} ${it.second.shortInfo}") },
+) {
+    UBasicBox {
+        Box(Modifier
+            .reportMeasuring("$tag outer", report)
+            .background(color)
+            .run { if (sizeRequired) requiredSize(size) else size(size) }
+            .reportMeasuring("$tag inner", report)
+        )
+    }
+}
+
+
+@Composable fun ReportsUi(modifier: Modifier = Modifier, reports: List<Pair<String, Any>>) {
     CompositionLocalProvider(LocalDensity provides Density(1f)) {
-        Column {
+        Column(modifier) {
             for ((key, data) in reports) {
                 Row(Modifier
                     .padding(2.dp)) {
@@ -46,53 +94,17 @@ fun MyLayoutExample1() {
     }
 }
 
-private val Any.shortInfo: String get() = when {
-    this is Placeable -> "Placeable(width=$width, height=$height)"
-    else -> this.toString()
-}
-
-@Composable
-private fun MyExaminedLayout(report: (Pair<String, Any>) -> Unit) {
-    UAlign(USTART, USTART) {
-        UBasicColumn {
-            // Box(Modifier.background(Color.Cyan))
-            UAlign(UEND, UEND) {
-                UBasicBox {
-                    Box(Modifier
-                        .reportMeasuring("green box", report)
-                        .background(Color.Green)
-                        .size(20.dp)
-                        .reportMeasuring("green box sized", report)
-                    )
-                }
-            }
-            UAlign(USTART, UEND) {
-                UBasicBox {
-                    Box(Modifier
-                        .background(Color.Magenta)
-                        .size(140.dp))
-                }
-            }
-            UAlign(UCENTER, UCENTER) {
-                UBasicBox {
-                    Box(Modifier
-                        .background(Color.Red)
-                        .requiredSize(110.dp))
-                }
-            }
-            // UAlign(USTRETCH, USTRETCH) { UBasicBox { Box(Modifier.background(Color.Blue).size(20.dp)) } }
-        }
-    }
+private val Any.shortInfo: String get() = when (this) {
+    is Placeable -> "Placeable(width=$width, height=$height, measuredWidth=$measuredWidth, measuredHeight=$measuredHeight)"
+    is String -> this
+    else -> toString()
 }
 
 fun Modifier.reportMeasuring(tag: String, report: (Pair<String, Any>) -> Unit): Modifier = layout { measurable, constraints ->
     report("$tag incoming constraints" to constraints)
     val placeable = measurable.measure(constraints)
-    report("$tag measured placeable" to placeable)
+    report("$tag measured placeable" to placeable.shortInfo)
     layout(placeable.width, placeable.height) {
         placeable.placeRelative(0, 0)
     }
 }
-
-
-@Preview @Composable fun MyLayoutPreview() = MyLayoutExample1()
