e704906336f321443ab328d7621815bc2abcddca
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-09-10 09:45:19 +0200

    fun

--- playgrounds-basic/src/androidTest/kotlin/playgrounds/MyPlaygroundsUSpek.kt
+++ playgrounds-basic/src/androidTest/kotlin/playgrounds/MyPlaygroundsUSpek.kt
@@ -1,6 +1,7 @@
 package pl.mareklangiewicz.playgrounds
 
 import androidx.compose.ui.test.junit4.*
+import kotlinx.coroutines.test.*
 import org.junit.*
 import org.junit.runner.*
 import pl.mareklangiewicz.ulog.*
@@ -12,5 +13,5 @@ class MyPlaygroundsUSpek {
     init { uspekLog = { ulogw("uspek ${it.status}") } }
     @get:Rule val rule = createComposeRule()
     private val scope = UComposeRuleScope(rule)
-    @USpekTestTree(33) fun melusf() = scope.MyExaminedLayoutUSpekFun(rule.density)
+    @USpekTestTree(33) fun melusf() = runTest { scope.MyExaminedLayoutUSpekFun(rule.density) }
 }
