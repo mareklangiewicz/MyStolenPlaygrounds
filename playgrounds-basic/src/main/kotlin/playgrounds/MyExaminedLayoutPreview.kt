a83d6bd74fe9fcbd984e754228e210b7df7b060a
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-28 18:56:03 +0200

    fun

--- playgrounds-basic/src/main/kotlin/playgrounds/MyExaminedLayoutPreview.kt
+++ playgrounds-basic/src/main/kotlin/playgrounds/MyExaminedLayoutPreview.kt
@@ -4,11 +4,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.*
 import pl.mareklangiewicz.udemo.*
 import pl.mareklangiewicz.uwidgets.UBinType.*
+import pl.mareklangiewicz.uwidgets.UWidgetsSki
 
 
-@Preview @Composable fun MyUBoxPreview() = MyExaminedLayoutPlayground(UBOX)
-@Preview @Composable fun MyURowPreview() = MyExaminedLayoutPlayground(UROW)
-@Preview @Composable fun MyUColumnPreview() = MyExaminedLayoutPlayground(UCOLUMN)
-@Preview @Composable fun MyAnimatedContentPreview() = MyAnimatedContentPlayground()
+@Preview @Composable fun MyUBoxPreview() = UWidgetsSki { MyExaminedLayoutPlayground(UBOX) }
+@Preview @Composable fun MyURowPreview() = UWidgetsSki { MyExaminedLayoutPlayground(UROW) }
+@Preview @Composable fun MyUColumnPreview() = UWidgetsSki { MyExaminedLayoutPlayground(UCOLUMN) }
+@Preview @Composable fun MyAnimatedContentPreview() = UWidgetsSki {  MyAnimatedContentPlayground() }
 
 
